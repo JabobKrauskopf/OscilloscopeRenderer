6124659acf84d9160646125e54d1a57753e7ad2d
Jakob Kraus <jakob.kraus10@gmail.com>
2020-06-06 01:04:03 +0200

    Resizing image

--- src/oscilloscope.py
+++ src/oscilloscope.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import pafy
 
-url = 'https://youtu.be/VYOjWnS4cMY'
+url = 'https://www.youtube.com/watch?v=OqJBloDEwwY'
 vPafy = pafy.new(url)
 play = vPafy.getbest()
 
@@ -12,7 +12,7 @@ while True:
     ret, frame = cap.read()
     edges = cv2.Canny(frame, 100, 200)
     emptyImage = np.zeros((len(frame), len(frame[0]), 3), dtype=np.uint8)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, maxLineGap=10, minLineLength=10)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 10, maxLineGap=1, minLineLength=1)
 
     try:
         for line in lines:
@@ -20,6 +20,7 @@ while True:
                 cv2.line(emptyImage, (x1, y1), (x2, y2), (255, 0, 0), 1)
     except TypeError:
         pass
+    emptyImage = cv2.resize(emptyImage, None, fx=1, fy=1)
     cv2.imshow("frame", emptyImage)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
